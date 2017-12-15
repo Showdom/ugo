55a3ba75d63c8afd8be1f998da72a9ad1fa26331
showdom <1063951161@qq.com>
2017-12-15 15:14:36 +0800

    ugo project.

--- setup.py
+++ setup.py
@@ -2,17 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='ugo',
-    version='0.0.2',
+    version='0.0.3',
     keywords=('ugo', 'tustack'),
     description='ugo base project.',
     long_description=open('README.rst').read(),
     py_modules=['ugo'],
     packages=find_packages(),
-    scripts=['say_hello.py'],
+    scripts=['scripts/shows.py'],
     install_requires=[],
     platforms=["all"],
     classifiers=[
-        'Development Status :: 1 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries',
         'License :: OSI Approved :: MIT License',
@@ -24,5 +23,5 @@ setup(
     license='MIT License',
     author='Showdom',
     author_email='1063951161@qq.com',
-    url='https://github.com/Showdom/ugo',
+    url='https://github.com/showdom/ugo',
 )
